e7f108f07337843af50647ceddbaf7a3af9761fa
mayank gupta <mayank.gupta@iontrading.com>
2018-01-17 20:24:43 +0530

    Added method to delete node by Index Value

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -16,7 +16,7 @@ function LinkedList() {
             this.headNode = new Node(data, null);
             this.tailNode = this.headNode;
         } else {
-            var newNode = new Node(data);
+            let newNode = new Node(data);
             newNode.next = this.headNode;
             this.headNode = newNode;
         }
@@ -28,7 +28,7 @@ function LinkedList() {
             this.headNode = new Node(data, null);
             this.tailNode = this.headNode;
         } else {
-            var newNode = new Node(data);
+            let newNode = new Node(data);
             this.tailNode.next = newNode;
             this.tailNode = newNode;
         }
@@ -42,9 +42,10 @@ function LinkedList() {
             this.headNode = null;
             this.tailNode = null;
         } else {
-            var currentNode = this.headNode;
+            let currentNode = this.headNode;
             this.headNode = null;
         }
+        this.nodeCount--;
     }
 
     LinkedList.prototype.removeFromEnd = () => {
@@ -54,14 +55,36 @@ function LinkedList() {
             this.headNode = null;
             this.tailNode = null;
         } else {
-            var currentNode = this.headNode;
+            let currentNode = this.headNode;
             while(currentNode.next != this.tailNode) {
                 currentNode = currentNode.next;
             }
             this.headNode = currentNode;
         }
+        this.nodeCount--;
     }
 
+    LinkedList.prototype.removeFromIndex = (index) => {
+        if(nodeCount) {
+            return "Empty List"
+        } else if(index > this.nodeCount) {
+            return "Node Does Not Exists"
+        } else if (index == 0 && this.nodeCount == 1) {
+            this.headNode = null;
+            this.tailNode = null;
+        } else if(index == 0){
+            this.removeFromStart();
+        } else if(index == (this.nodeCount - 1)) {
+            this.removeFromEnd();
+        } else {
+            let currentNode = this.headNode;
+            for(let i = 0; i<index; i++) {
+                currentNode = currentNode.next;
+            }
+            let nodeToBeDeleted = currentNode.next;
+            currentNode = nodeToBeDeleted.next;
+        }
+    }
     return {
         LinkedList: LinkedList
     }
